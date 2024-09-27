0fff92cffe4a3f6698e5aba2a1b18e8f0bbab6b9
TAMANNA <tamannakarthol320@gmail.com>
2024-09-27 00:23:53 -0700

    add

---
+++ Frontend/src/components/Navbar.jsx
@@ -0,0 +1,50 @@
+// import React from 'react'
+// import { Link } from 'react-router-dom'
+
+// const Navbar = () => {
+
+//     const navItems =[
+//         {
+//             name:'Home',
+//             link:'/'
+//         },
+//         {
+//             name:'About Us',
+//             link:'/about'
+//         },
+//         {
+//             name:'Find Blood',
+//             link:'/find_blood'
+//         },
+//         {
+//             name:'Register',
+//             link:'/register'
+//         },
+
+//     ]
+
+//   return (
+//     <>
+//       <h1>hello</h1>
+//       <header>
+//         <nav>
+//             <div>
+
+//             </div>
+//             <ul>
+//                 {navItems.map((item)=>(
+//                     <Link key={item.link} to={item.link}>
+//                         <ul>
+//                             <li>{item.name}</li>
+//                         </ul>
+//                     </Link>
+                    
+//                 ))}
+//             </ul>
+//         </nav>
+//       </header>
+//     </>
+//   )
+// }
+
+// export default Navbar
