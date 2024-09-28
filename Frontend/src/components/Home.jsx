fa852fbdd5d6c3acf549871d07dd1c2d2d2145af
TAMANNA <tamannakarthol320@gmail.com>
2024-09-27 23:06:39 -0700

    add

---
+++ Frontend/src/components/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import bgimg from '../assets/Background image/Ellipse 8.svg'
+const Home = () => {
+  return (
+    <div >
+      <div>
+        <img src={bgimg} alt="" className='w-[40%] absolute top-0'/>
+      </div>
+    </div>
+  )
+}
+
+export default Home
--- Frontend/src/components/Navbar.jsx
+++ Frontend/src/components/Navbar.jsx
@@ -1,50 +1,67 @@
-// import React from 'react'
-// import { Link } from 'react-router-dom'
+import React from 'react'
 
-// const Navbar = () => {
+const Navbar = () => {
+  return (
+    <div>
+      <div className="navbar bg-transparent z-[1000]">
+  <div className="navbar-start">
+    <div className="dropdown">
+      <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          className="h-5 w-5"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor">
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth="2"
+            d="M4 6h16M4 12h8m-8 6h16" />
+        </svg>
+      </div>
+      <ul
+        tabIndex={0}
+        className="menu menu-sm dropdown-content bg-transparent rounded-box z-[1] mt-3 w-52 p-2 shadow">
+        <li><a>Home</a></li>
+        <li><a href="">About Us</a></li>
+        <li>
+          <a>Register</a>
+          <ul className="p-2">
+            <li><a>Donor</a></li>
+            <li><a>Patient</a></li>
+            <li><a href="">Organization</a></li>
+          </ul>
+        </li>
+        <li><a>Find Blood</a></li>
+      </ul>
+    </div>
+    <a className="btn btn-ghost text-xl">Blood Donation</a>
+  </div>
+  <div className="navbar-center hidden lg:flex">
+    <ul className="menu menu-horizontal px-1">
+      <li><a>Home</a></li>
+      <li><a href="">About Us</a></li>
 
-//     const navItems =[
-//         {
-//             name:'Home',
-//             link:'/'
-//         },
-//         {
-//             name:'About Us',
-//             link:'/about'
-//         },
-//         {
-//             name:'Find Blood',
-//             link:'/find_blood'
-//         },
-//         {
-//             name:'Register',
-//             link:'/register'
-//         },
+      <li>
+        <details>
+          <summary>Register</summary>
+          <ul className="p-2">
+            <li><a>Donor</a></li>
+            <li><a>Patient</a></li>
+            <li><a href="">organization</a></li>
+          </ul>
+        </details>
+      </li>
+      <li><a>Find Blood</a></li>
+    </ul>
+  </div>
+  <div className="navbar-end">
+    <a className="btn bg-red-500 text-white">Donate Blood</a>
+  </div>
+</div>
+    </div>
+  )
+}
 
-//     ]
-
-//   return (
-//     <>
-//       <h1>hello</h1>
-//       <header>
-//         <nav>
-//             <div>
-
-//             </div>
-//             <ul>
-//                 {navItems.map((item)=>(
-//                     <Link key={item.link} to={item.link}>
-//                         <ul>
-//                             <li>{item.name}</li>
-//                         </ul>
-//                     </Link>
-                    
-//                 ))}
-//             </ul>
-//         </nav>
-//       </header>
-//     </>
-//   )
-// }
-
-// export default Navbar
+export default Navbar
