5f03ca9cd3eadbb939136a63a095d1b970369b89
Arpit <156578394+arpitbhatia23@users.noreply.github.com>
2024-09-28 11:38:32 +0530

    add

--- Frontend/src/components/Home.jsx
+++ Frontend/src/components/Home.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 import bgimg from '../assets/Background image/Ellipse 8.svg'
 const Home = () => {
   return (
-    <div >
+    <div className=''>
       <div>
-        <img src={bgimg} alt="" className='w-[40%] absolute top-0'/>
+        <img src={bgimg} alt="" className='w-[40%] absolute top-0 z-[-100]'/>
       </div>
     </div>
   )
