ccd229a94b3a52066475de09ce253f258d78c0b6
TAMANNA <tamannakarthol320@gmail.com>
2024-09-29 23:26:17 -0700

    add

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -3,11 +3,8 @@ import Navbar from './components/Navbar'
 import Home from './components/Home'
 import Mission from './components/Mission'
 import Carousel from './components/Carousel'
-<<<<<<< HEAD
 import GetBlood from './components/GetBlood'
-=======
 import Footer from './components/Footer'
->>>>>>> 4efefc4a36d9ae877aeb6b7f2022b75b48e23148
 
 const App = () => {
   return (
@@ -16,12 +13,9 @@ const App = () => {
       <Home/>
       <Mission/>
       <Carousel/>
-<<<<<<< HEAD
       <GetBlood/>
-=======
 
       <Footer/>
->>>>>>> 4efefc4a36d9ae877aeb6b7f2022b75b48e23148
     </div>
   )
 }
--- Frontend/src/components/GetBlood.jsx
+++ Frontend/src/components/GetBlood.jsx
@@ -4,23 +4,59 @@ import img1 from '../assets/logo/bi_pencil-square.svg'
 const GetBlood = () => {
   return (
     <div>
-      <div className='border border-black p-8' >
-        <h1 className='text-3xl font-bold text-center'>How to Get Blood</h1>
-        <div className='border border-red-600 p-4'>
-          <div className='border border-blue-500'>
-          <div className='border border-black w-16  rounded-full text-center  p-4 relative'>
+      <div className='border border-black p-16 my-16' >
+        <h1 className='flex flex-col text-3xl font-bold text-center'>How to Get Blood</h1>
+        <div className='flex flex-col items-center gap-16 border border-red-600 p-4 my-16'>
+            {/* ============================================= */}
+          <div className='flex flex-col  border border-blue-500 w-max '>
+          <div className='flex flex-col  border border-black w-16  rounded-full text-center  p-4 relative '>
                 <h1 >1</h1>
-                <div className='bg-white rounded-full border border-black w-96  absolute'>
-                    <div>
-                        <img src={img1} alt="" className='h-[20%] w-[20%]'/>
+                <div className='bg-white rounded-full border border-black w-56 h-56  p-8
+                  absolute '>
+                    <div className='rounded-full'>
+                        <img src={img1} alt="" className='w-16 m-auto '/>
                     </div>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing.</p>
+                    <p className=''>Lorem ipsum dolor sit amet consectetur adipisicing.</p>
+
+                </div>
+            </div>
+          </div>
+          </div>
+{/* ========================================== */}
+<div className='flex   justify-evenly items-center gap-8'>
+<div className='flex flex-col  border border-blue-500 w-max '>
+          <div className='flex flex-col  border border-black w-16  rounded-full text-center  p-4 relative '>
+                <h1 >2</h1>
+                <div className='bg-white rounded-full border border-black w-56 h-56  p-8
+                  absolute '>
+                    <div className='rounded-full'>
+                        <img src={img1} alt="" className='w-16 m-auto '/>
+                    </div>
+                    <p className=''>Lorem ipsum dolor sit amet consectetur adipisicing.</p>
+
+                </div>
+            </div>
+          </div>
+          {/* ========================================== */}
+
+          <div className='flex flex-col  border border-blue-500 w-max '>
+          <div className='flex flex-col  border border-black w-16  rounded-full text-center  p-4 relative '>
+                <h1 >3</h1>
+                <div className='bg-white rounded-full border border-black w-56 h-56  p-8
+                  absolute '>
+                    <div className='rounded-full'>
+                        <img src={img1} alt="" className='w-16 m-auto '/>
+                    </div>
+                    <p className=''>Lorem ipsum dolor sit amet consectetur adipisicing.</p>
 
                 </div>
             </div>
           </div>
 
-        </div>
+          </div>
+          
+
+       
 
       </div>
     </div>
