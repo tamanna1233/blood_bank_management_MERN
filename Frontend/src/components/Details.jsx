e54991d2787ed79c10e39d39940c5372d40a93fa
TAMANNA <tamannakarthol320@gmail.com>
2024-10-09 07:00:39 -0700

    add

--- Frontend/src/components/Details.jsx
+++ Frontend/src/components/Details.jsx
@@ -8,8 +8,8 @@ const Details = () => {
             <img src={logo} alt="" />
         </div>
         
-      <div className=' w-[50%]'>
-      <h1 className='  font-bold text-xl text-black p-2'>Recipient details </h1> <br />
+      <div className='w-[100%] sm:w-[50%]'>
+      <h1 className=' flex flex-col items-center font-bold text-xl text-black p-2'>Recipient details </h1> <br />
         <form action="" className='flex flex-col'>
         <label htmlFor="">Blood Type</label>
         <select name="" id="" className='border border-black rounded-lg m-1  '>
@@ -25,7 +25,7 @@ const Details = () => {
             </select>
 <br />
             {/* ======current location ==== */}
-            <button className='bg-black text-white  font-bold text-xl  rounded-xl p-1  w-[50%] mx-auto '>Current Location</button>
+            <button className='bg-black text-white  font-bold text-xl  rounded-xl p-1  w-[50%] mx-auto hover:bg-gray-500 hover:border border-black'>Current Location</button>
             <br />
             <hr />
             <h1 className='text-center'>OR</h1>
@@ -38,6 +38,10 @@ const Details = () => {
            </div>
            <button className='bg-black text-white text-lg font-bold rounded-lg mx-auto px-2 p-1 hover:bg-gray-500 hover:border border-black'>Proceed</button>
         </form>
+        <div className="join join-vertical  border border-black rounded-2xl lg:join-horizontal ">
+  <button className="hover:bg-black hover:text-white rounded-2xl p-1 active:bg-black">Blood Donors</button>
+  <button className="hover:bg-black hover:text-white  rounded-2xl p-1 active:bg-black">Blood Banks</button>
+</div>
       </div>
     </div>
   )
